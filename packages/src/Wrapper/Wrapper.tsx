b73c7e4391cf4ad1a99cf2609d9159323205e285
Gustavo <gustavo.aguiar@redspark.io>
2022-02-23 10:53:38 -0300

    a

--- packages/src/Wrapper/Wrapper.tsx
+++ packages/src/Wrapper/Wrapper.tsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Wrapper = styled.section`
-  padding: 4em;
+  padding: 5em;
   background: papayawhip;
-`;
+`
 
 export default Wrapper
