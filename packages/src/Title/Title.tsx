5e65d7a4e23e4326e2f9cc53f96c19e290ebe480
Gustavo <gustavo.aguiar@redspark.io>
2022-02-23 10:54:41 -0300

    a

--- packages/src/Title/Title.tsx
+++ packages/src/Title/Title.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: 1.5rem;
   text-align: center;
   color: palevioletred;
-`;
+`
 
 type Props = {
   children: React.ReactNode
 }
 
-export default ({children}: Props) => {
-  return (<Title>
-    {children}
-  </Title>)
-};
+export default ({ children }: Props) => {
+  return <Title>{children}</Title>
+}
